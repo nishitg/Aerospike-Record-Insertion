c96715b2d7ebb1f2b7b57f8d43cc877566a5b48b
B0206601 <b0206601@LTB0206601-MAC.local>
2019-09-13 12:29:34 +0530

    Aerospike Configuration file and Single Record JSON Value

---
+++ configs.py
@@ -0,0 +1,20 @@
+user_name="admin"
+password="password"
+namespace="arch"
+set="sample1"
+hosts=[('127.0.0.1',3000)]
+PK_field="City"
+
+json = {
+   "LatD": 41,
+   "LatM": 5,
+   "LatS": 59,
+   "NS": "N",
+   "LonD": 80,
+   "LonM": 39,
+   "LonS": 0,
+   "EW": "W",
+   "City": "Youngstown",
+   "State": " OH"
+}
+
