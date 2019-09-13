9bf28df215351fb9d11ec9004e239509713b606c
B0206601 <b0206601@LTB0206601-MAC.local>
2019-09-13 12:30:07 +0530

    Insert Single Record in Aerospike

---
+++ insert_csv.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import aerospike
+from aerospike import exception as ex
+import sys
+import string
+from multiprocessing import Process, Lock, Pool
+import time
+import configs
+import csv
+
+def closeClient (client):
+	client.close()
+
+def insertInAS(client,record,asParams):
+    try:
+        key = (asParams['namespace'], asParams['set'], record[configs.PK_field])
+        client.put(key,record)
+	print ("Inserted", record[configs.PK_field])
+    except ex.RecordError as e:
+        print("Error: {} [{}]".format(e.msg, e.code))
+    except Exception as e:
+        import traceback
+        print(traceback.print_exc())
+
+def connectToAS(config, asParams):
+    try:
+        client = aerospike.client(config).connect(asParams['user_name'], asParams['password'])
+        return client
+    except ex.ClientError as e:
+        print("Error: {0} [{1}]".format(e.msg, e.code))
+        sys.exit(1)
+
+def parseRecord():
+	record = configs.json
+	return record
+
+def setParams():
+	asParams = {}
+	asParams['user_name']=configs.user_name
+	asParams["password"]=configs.password
+	asParams["namespace"]=configs.namespace
+	asParams["set"]=configs.set
+	
+	hosts = configs.hosts
+	config = {'hosts': hosts}
+	write_policies = {'total_timeout': 20000, 'max_retries': 10}
+	read_policies = {'total_timeout': 15000, 'max_retries': 10}
+	policies = {'write': write_policies, 'read': read_policies}
+	config['policies'] = policies
+	
+	return config, asParams
+
+def main():
+	config, asParams = setParams()
+	record = parseRecord()
+	client = connectToAS(config, asParams)
+	f = csv.DictReader(open("records.csv","r"))
+	for line in f:
+		insertInAS(client,line,asParams)
+	closeClient(client)
+	
+if __name__ == '__main__':
+    main()
+
